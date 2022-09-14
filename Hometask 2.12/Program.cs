8db9e642c9baf49d89ac6015cb9bf0ebabf000d5
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-09-14 22:12:30 +0300

    Add project files.

---
+++ Hometask 2.12/Program.cs
@@ -0,0 +1,25 @@
+﻿// Hometask 2.12
+
+// 1. Create queries using:
+//        - Group (by multiple fields)
+//        - GroupJoin
+//        - Aggregations
+//        - Having
+// 2. Create subqueries with:
+//        - In
+//        - Exists
+//        - Any
+//        - All
+// 3. Create queries with projection conditionals
+// 4. Write extension methods that does paginating for me
+
+namespace App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
